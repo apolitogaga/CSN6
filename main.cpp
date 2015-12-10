1e25837f4cb848e550aa3c83b712fcd75821aae5
PRudnik <pawelrudnik92@o2.pl>
2015-12-10 22:13:26 +0100

    Task 3.1 done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <algorithm>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
@@ -29,7 +30,7 @@ void create_degree_sequence_from_stubs(vector<int> stubs, int t)
     stringstream ss;
 
     //Prepare filename
-    ss << "C:\\users\\Pawel\\Desktop\\degree_";
+    ss << "C:\\users\\Pawel\\Desktop\\BA_degree_";
     if(t<TMAX) ss  << t;
     else ss << "sequence";
     ss << ".txt";
@@ -46,9 +47,38 @@ void create_degree_sequence_from_stubs(vector<int> stubs, int t)
     out.close();
 }
 
+void save_degrees(vector<int> degrees, int t)
+{
+    //If it should be final degree sequence, sort degrees descending
+    if(t==TMAX) sort(degrees.begin(),degrees.end(), greater<int>());
+
+    stringstream ss;
+
+    //Prepare filename
+    ss << "C:\\users\\Pawel\\Desktop\\RG_degree_";
+    if(t<TMAX) ss  << t;
+    else ss << "sequence";
+    ss << ".txt";
+
+    string filename = ss.str();
+
+    ofstream out(filename);
+
+    for(int i=0;i<degrees.size();i++)
+    {
+        out << degrees[i] << endl;
+    }
+
+    out.close();
+}
+
 int main()
 {
     default_random_engine generator;
+
+    //##################################
+    //##   PART 1 - BARABASI-ALBERT   ##
+    //##################################
 
     vector<int> stubs;
 
@@ -58,6 +88,10 @@ int main()
         stubs.push_back(i);
         stubs.push_back(i+1);
     }
+
+    //Closes the circle
+    stubs.push_back(N0);
+    stubs.push_back(1);
 
     int node = N0;
     int index = 0;
@@ -78,7 +112,7 @@ int main()
             while(true)
             {
                 index = distribution(generator);
-                //If it is the first stub break
+                //If it is the first stub chosen break
                 if(chosen.empty()) break;
                 //If it was not chosen break
                 if(find(chosen.begin(), chosen.end(), stubs[index]) == chosen.end()) break;
@@ -100,6 +134,57 @@ int main()
     }
 
     create_degree_sequence_from_stubs(stubs,TMAX);
+
+
+
+    //##################################
+    //##    PART 2 - RANDOM GROWTH    ##
+    //##################################
+
+    //Creates initial graph in form of degrees set
+    vector<int> degrees;
+    for(int i=0; i<N0; i++)
+    {
+        degrees.push_back(2);
+    }
+
+    for(int t=1; t<=TMAX; t++)
+    {
+        chosen.clear();
+
+        //Creates random number distribution
+        uniform_int_distribution<int> distribution(0, degrees.size()-1);
+
+        //Looks for M0 different vertices to connect
+        for(int m=0; m<M0; m++)
+        {
+            //Looks for random vertex which was not chosen before
+            while(true)
+            {
+                index = distribution(generator);
+                //If it is the first vertex chosen break
+                if(chosen.empty()) break;
+                //If it was not chosen break
+                if(find(chosen.begin(), chosen.end(), index) == chosen.end()) break;
+                //Else try again
+            }
+
+            //Increase degree of found vertex
+            degrees[index]++;
+            //Save found vertex
+            chosen.push_back(index);
+        }
+
+        //Add new node with initial degree M0
+        degrees.push_back(M0);
+
+        if(t==1||t==10||t==100||t==1000)
+        {
+            save_degrees(degrees, t);
+        }
+    }
+
+    save_degrees(degrees,TMAX);
 
     return 0;
 }
