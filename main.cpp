313e9e3aef89694245f89f58f006adafe584970e
PRudnik <pawelrudnik92@o2.pl>
2015-12-11 02:53:08 +0100

    Task 4.1 done

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-const int N0 = 100;
+const int N0 = 1000;
 const int M0 = 10;
 const int TMAX = 10000;
 
@@ -56,6 +56,40 @@ void save_degrees(vector<int> degrees, int t)
 
     //Prepare filename
     ss << "C:\\users\\Pawel\\Desktop\\RG_degree_";
+    if(t<TMAX) ss  << t;
+    else ss << "sequence";
+    ss << ".txt";
+
+    string filename = ss.str();
+
+    ofstream out(filename);
+
+    for(int i=0;i<degrees.size();i++)
+    {
+        out << degrees[i] << endl;
+    }
+
+    out.close();
+}
+
+void create_degree_from_adjacency_list(map<int,vector<int>> adjacency_list, int t)
+{
+    vector<int> degrees;
+
+    //Creates degree sequence by counting each node connections
+    typedef map<int, vector<int>>::iterator it_type;
+    for(it_type iterator = adjacency_list.begin(); iterator != adjacency_list.end(); iterator++)
+    {
+       degrees.push_back(iterator->second.size());
+    }
+
+    //If it should be final degree sequence, sort degrees descending
+    if(t==TMAX) sort(degrees.begin(),degrees.end(), greater<int>());
+
+    stringstream ss;
+
+    //Prepare filename
+    ss << "C:\\users\\Pawel\\Desktop\\NG_degree_";
     if(t<TMAX) ss  << t;
     else ss << "sequence";
     ss << ".txt";
@@ -185,6 +219,68 @@ int main()
     }
 
     save_degrees(degrees,TMAX);
+
+
+
+    //##################################
+    //##      PART 3 - NO GROWTH      ##
+    //##################################
+
+    //Creates initial graph in form of adjacency list
+    int before;
+    int after;
+    map<int, vector<int>> adjacency_list;
+    for(int i=0;i<N0;i++)
+    {
+        if(i==0) before = N0-1;
+        else before = i-1;
+        if(i==N0-1) after = 0;
+        else after = i+1;
+
+        vector<int> connections;
+        connections.push_back(before);
+        connections.push_back(after);
+
+        adjacency_list.insert(pair<int, vector<int>>(i,connections));
+    }
+
+    int vertex;
+    for(int t=1; t<=TMAX; t++)
+    {
+        chosen.clear();
+
+        //Creates random number distribution
+        uniform_int_distribution<int> distribution(0, adjacency_list.size()-1);
+        //Choose randomly one of vertices
+        vertex = distribution(generator);
+
+        vector<int> conn = adjacency_list.find(vertex)->second;
+
+        //Looks for M0 different vertices to connect
+        for(int m=0; m<M0; m++)
+        {
+
+            while (true)
+            {
+                //Look for another vertex
+                index = distribution(generator);
+                //If they were not connected break
+                if (find(conn.begin(), conn.end(), index) == conn.end()) break;
+                //Else try again
+            }
+
+            //Add connections
+            adjacency_list.find(vertex)->second.push_back(index);
+            adjacency_list.find(index)->second.push_back(vertex);
+        }
+
+        if(t==1||t==10||t==100||t==1000)
+        {
+            create_degree_from_adjacency_list(adjacency_list,t);
+        }
+    }
+
+    create_degree_from_adjacency_list(adjacency_list,TMAX);
 
     return 0;
 }
